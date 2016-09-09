97d4ef0c646cd5e3d1c5db9bb866a0ed0f7f4111
Jerry Mannel <jerry@capiot.com>
2016-09-09 16:31:30 +0530

    v.1.0

---
+++ app.js
@@ -0,0 +1,121 @@
+var redis = require('redis'),
+    os = require('os'),
+    assert = require('assert')
+
+var client = null
+var basePath = null
+
+function getIPAddress(interface) {
+    // 1. first check env variable PUTTU_PORT - process.env.PUTTU_INT
+    // 2. then check the interface
+    // 3. NOne of the above, fall back to eth0
+    if (!process.env.PUTTU_IP) {
+        if (interface) {
+            //console.log(os.networkInterfaces()[interface][0].address)
+            return os.networkInterfaces()[interface][0].address
+        } else {
+            //console.log(os.networkInterfaces()["eth0"][0].address)
+            return os.networkInterfaces()['eth0'][0].address
+        }
+    } else {
+        //console.log(os.networkInterfaces()[process.env.PUTTU_INT][0].address)
+        return process.env.PUTTU_IP
+    }
+}
+
+function connect(_host, _port) {
+    assert.notEqual(_host, undefined, 'Empty host name')
+    assert.notEqual(_port, undefined, 'Empty port number')
+
+    client = redis.createClient(_port, _host)
+
+    client.on("error", function (err) {
+        console.log('Redis Disconnected, stopping service')
+        process.exit(0)
+    })
+}
+
+function addToRedis(_path, _data) {
+    try {
+
+        client.ttl(_path + "_set", (e, r) => {
+            if (e) {
+                console.log(e);
+                process.exit(0);
+            }
+            if (r == -2 || r == -1) {
+                client.sadd(_path, _data);
+                client.pexpire(_path, 1000)
+            }
+            setTimeout(function () {
+                addToRedis(_path, _data)
+            }, 500)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function setMagicKey(_key) {
+    return new Promise((res, rej) => {
+        client.hget("magicwords", _key, (_e, _d) => {
+            if (_e) rej(_e)
+            if (!_d) {
+                client.hset("magicwords", _key, new Date().toISOString(), (_e, _d) => {
+                    if (_e) rej(_e)
+                    res()
+                })
+            } else res()
+        })
+    });
+}
+
+function getMagicKey(_key) {
+    return new Promise((res, resj) => {
+        client.hget("magicwords", _key, (_e, _d) => {
+            if (_e) rej(_e)
+            res(_d)
+        })
+    });
+}
+
+function register(_path, _data, _interface) {
+    return new Promise((resolve, reject) => {
+        if (!_path) reject('Missing config path.')
+        if (!_data) reject('Missing config value')
+
+        var redisSet = _path + "_set"
+        setMagicKey(_path).then(() => {
+            var data = _data.protocol.toLowerCase() + '://' + getIPAddress(_interface) + ':' + _data.port + _data.api
+            addToRedis(redisSet, data)
+            resolve()
+        }, () => {
+            log("error")
+        })
+    })
+}
+
+function get(_path) {
+    var redisSet = _path + "_set"
+    return new Promise((resolve, reject) => {
+        client.srandmember(redisSet, (_e, _d) => {
+            if (_e || !_d) {
+                reject(_e)
+            }
+            resolve(_d)
+        })
+    })
+}
+
+var counter = 1;
+
+function log(s) {
+    console.log(counter + " " + s);
+    counter += 1;
+}
+
+
+exports.connect = connect
+exports.register = register
+exports.get = get
+exports.getMagicKey = getMagicKey
---
+++ test.js
@@ -0,0 +1,23 @@
+var puttu = require('./app.js')
+
+puttu.connect('localhost', '6379')
+
+var master = "test"
+puttu.register(master, {
+    protocol: 'http',
+    port: '10001',
+    api: '/brand/v1'
+}, null).then(
+    () => {
+        console.log('Registered self')
+        puttu.get(master).then(
+            d => console.log(d),
+            e => console.log(e)
+        );
+        puttu.getMagicKey(master).then(
+            d => console.log(d),
+            e => console.log(e)
+        )
+    },
+    e => console.log(e)
+);
